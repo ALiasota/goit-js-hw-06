6f6d0f7ee83097c7d9aaf3d4443c9c04eb06859b
Andrii Liasota <lyasota.andriy@gmail.com>
2022-01-14 16:32:11 +0200

    9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
+
+
+
+form.addEventListener("submit", onFormSubm);
+
+function onFormSubm (event) {
+    event.preventDefault();
+    const {
+        elements: {email, password}
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("все поля должны быть заполнены");
+        return;
+    }
+    const user = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(user);
+
+    event.currentTarget.reset();
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const text = document.querySelector(".color");
+
+const button = document.querySelector(".change-color");
+
+const body = document.body;
+
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  text.textContent = color;
+}
